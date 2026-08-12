6a2e439f4635285730379609ef3a0324d811d6cd
northantsdealspot-lgtm <northantsdealspot@gmail.com>
2026-08-12 19:26:16 +0100

    Fix offline asset paths for GitHub Pages

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='blackledgerstone-v2.1.0-intel';
-const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/raven-192.png','./icons/raven-512.png'];
+const CACHE='blackledgerstone-v2.1.1-github';
+const CORE=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./raven-192.png','./raven-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
